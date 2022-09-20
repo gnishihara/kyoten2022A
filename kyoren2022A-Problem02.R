aeff4957720b2115302f329f27ab03f448766875
Greg Nishihara <gnishihara@gmail.com>
2022-09-20 14:53:42 +0900

    update

--- kyoren2022A-Problem02.R
+++ kyoren2022A-Problem02.R
@@ -6,11 +6,9 @@
 # tidyverse の紹介
 library(tidyverse)
 library(lubridate)
+library(showtext)
 
 # コンマ区切りファイルの読み込み関数
-# read.csv() # base R　関数
-read_csv() # tidyverse (readr) 関数
-
 url1 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_201007.csv"
 url2 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_201110.csv"
 url3 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_201223.csv"
@@ -18,19 +16,12 @@ url4 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Mic
 url5 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_210528.csv"
 url6 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_220116.csv"
 
-# path1 = "./data/Microstation_00_ecser_surface_201007.csv"
-
 ds1 = read_csv(file = url1, skip = 1)
-# ds1 = read_csv(file = path1, skip = 1)
 ds2 = read_csv(file = url2, skip = 1)
 ds3 = read_csv(file = url3, skip = 1)
 ds4 = read_csv(file = url4, skip = 1)
 ds5 = read_csv(file = url5, skip = 1)
 ds6 = read_csv(file = url6, skip = 1)
-
-ds1 # tidyverse の data frame は　tibble 
-
-# matches() は正規表現で変数名を探します
 
 ds1 = rename(ds1, 
              n = `#`,
@@ -79,28 +70,9 @@ ds6 = rename(ds6,
              wind = matches("風速"),
              gust = matches("突風"),
              pressure = matches("mbar"))
-
-ds6 # 最初の10行
-tail(ds6) # 最後の６行
-
-
-# |> # パイプ記号 CTRL+SHIFT+M
-ds6 |> tail()
-
-ds6 |> slice(1:12) |> tail() # パイプにの連結
-tail(slice(ds6, 1:12))       # 関数のネスティング
-
-sample(1:6, size =  3) |> mean()
-
 # 複数データの結合
-
-# ds1 4911 x 6
-# ds2 6200 x 6
 dset = bind_rows(ds1, ds2, ds3, ds4, ds5, ds6)
 
-# lubridate::parse_date_time()
-
-# 時間データを文字列から時間データへ変換する
 dset = dset |> 
   mutate(datetime = parse_date_time(datetime,
                                     orders = "mdyT"))
@@ -109,23 +81,14 @@ dset = dset |>
   mutate(datetime = floor_date(datetime, 
                                unit = "minutes"))
 
-# 一日におけるデータ数
-6 * 24
-
 dset = dset |> 
   mutate(day = floor_date(datetime, 
                           unit = "days"))
-
-dset |> 
-  group_by(day) |> 
-  filter(length(day) < (6 * 24)) 
-
 # 144 点のデータだけ残す
 dset = dset |> 
   group_by(day) |> 
   filter(near(length(day), 6 * 24)) 
 
-dset
 
 # day ごとの平均値
 
@@ -135,4 +98,25 @@ dset_summary = dset |>
             gust = mean(gust),
             par  = mean(par),
             pressure = mean(pressure))
+#############################################################################
+
+ggplot(dset_summary) + 
+  geom_point(aes(x = pressure,
+                 y = wind)) +
+  scale_x_continuous(name = "平均気圧 (mbar)",
+                     breaks = c(990, 1000, 1010, 1020, 1030, 1040),
+                     limits = c(990, 1040)) +
+  scale_y_continuous(name = "平均風速 (m / s)",
+                     breaks = seq(0, 8, by = 2),
+                     limits = c(0, 8))
+
+
+ggsave("mbar-wind.pdf", width = 80, height = 80, units = "mm")
+
+
+# 問題：par ~ pressure, wind ~ gust の図をつくる。
+# ファイル名： "namae-par-wind.pdf" か "namae-wind-gust.pdf"
+
+
+
 
