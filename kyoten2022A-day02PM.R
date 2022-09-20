34449c6b13cb6ba9924a20a0c759bd9af5cf23c3
Greg Nishihara <gnishihara@gmail.com>
2022-09-20 14:31:04 +0900

    update

--- kyoten2022A-day02PM.R
+++ kyoten2022A-day02PM.R
@@ -4,6 +4,7 @@
 
 # tidyverse の紹介
 library(tidyverse)
+library(lubridate)
  
 # コンマ区切りファイルの読み込み関数
 # read.csv() # base R　関数
@@ -16,7 +17,10 @@ url4 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Mic
 url5 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_210528.csv"
 url6 = "https://raw.githubusercontent.com/gnishihara/kyoten2022A/master/data/Microstation_00_ecser_surface_220116.csv"
 
+# path1 = "./data/Microstation_00_ecser_surface_201007.csv"
+
 ds1 = read_csv(file = url1, skip = 1)
+# ds1 = read_csv(file = path1, skip = 1)
 ds2 = read_csv(file = url2, skip = 1)
 ds3 = read_csv(file = url3, skip = 1)
 ds4 = read_csv(file = url4, skip = 1)
@@ -75,11 +79,83 @@ ds6 = rename(ds6,
              gust = matches("突風"),
              pressure = matches("mbar"))
 
-# データの読み方
+ds6 # 最初の10行
+tail(ds6) # 最後の６行
 
-# データの操作
+
+# |> # パイプ記号 CTRL+SHIFT+M
+ds6 |> tail()
+
+ds6 |> slice(1:12) |> tail() # パイプにの連結
+tail(slice(ds6, 1:12))       # 関数のネスティング
+  
+sample(1:6, size =  3) |> mean()
+
+# 複数データの結合
+
+# ds1 4911 x 6
+# ds2 6200 x 6
+dset = bind_rows(ds1, ds2, ds3, ds4, ds5, ds6)
+
+# lubridate::parse_date_time()
+
+# 時間データを文字列から時間データへ変換する
+dset = dset |> 
+  mutate(datetime = parse_date_time(datetime,
+                              orders = "mdyT"))
+# 時間データの切り捨て処理
+dset = dset |> 
+  mutate(datetime = floor_date(datetime, 
+                              unit = "minutes"))
+
+# 一日におけるデータ数
+6 * 24
+
+dset = dset |> 
+  mutate(day = floor_date(datetime, 
+                          unit = "days"))
+
+dset |> 
+  group_by(day) |> 
+  filter(length(day) < (6 * 24)) 
+
+# 144 点のデータだけ残す
+dset = dset |> 
+  group_by(day) |> 
+  filter(near(length(day), 6 * 24)) 
+
+dset
+
+# day ごとの平均値
+
+dset_summary = dset |> 
+  group_by(day) |> 
+  summarise(wind = mean(wind),
+            gust = mean(gust),
+            par  = mean(par),
+            pressure = mean(pressure))
+
 
 # 作図
+
+
+ggplot(dset_summary) + 
+  geom_point(aes(x = day,
+                 y = pressure))
+
+ggplot(dset_summary) + 
+  geom_point(aes(x = pressure,
+                 y = wind))
+
+
+
+
+
+
+
+
+
+
 
 
 
