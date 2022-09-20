c86e5ba5d11f2f1dd0c9c43c5941619fc3fcf93e
Greg Nishihara <gnishihara@gmail.com>
2022-09-20 09:41:53 +0900

    update

--- kyoten2022A-day02AM.R
+++ kyoten2022A-day02AM.R
@@ -20,14 +20,39 @@ iris$Sepal.Length == find_length
 
 # dplyr　パッケージの near() 関数を使う。
 dplyr::near(1/3, 0.33, tol = 0.01) # dplyr の関数
+identical(1/3, 0.3)  # Base R の関数
 all.equal(1/3, 0.33) # Base R の関数
 
 iris$Sepal.Length <= find_length
 iris$Sepal.Length != find_length
 
+## 論理ベクトルをつかって
+## データを抽出する
+
+logical_sl = iris$Sepal.Length > find_length   # よりおおきい
+
+iris$Sepal.Length[1]
+iris$Sepal.Length[2]
+iris$Sepal.Length[c(1, 5)]
+
+iris$Sepal.Length[logical_sl]
+iris$Sepal.Length[!logical_sl]
+
+iris$Petal.Length[iris$Petal.Length > 10] # numeric(0) が帰ってくる
+iris$Petal.Length[iris$Petal.Length > 6.5] 
 
 
+logical01 = iris$Petal.Length >= 5
+logical02 = iris$Petal.Length <= 6
+
+iris$Petal.Length[logical01] 
+iris$Petal.Length[logical02] 
+
+# 5 >= Petal.Length >= 6
+iris$Petal.Length[logical01 & logical02] 
+iris$Petal.Length[iris$Petal.Length >= 5 & iris$Petal.Length <= 6] 
  
+
 # R関数の作り方（サイコロ）
 
 
