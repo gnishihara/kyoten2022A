b4d3111496c841bbe9318c4a31126ed4182d2168
Greg Nishihara <gnishihara@gmail.com>
2022-09-21 09:06:03 +0900

    update

--- kyoten2022A-day03AM.R
+++ kyoten2022A-day03AM.R
@@ -5,7 +5,27 @@
 # tidyverse の紹介
 library(tidyverse)
 library(lubridate)
+library(magick)
+library(showtext)
 
+################################################################
+# 日本語フォントの準備
 
+font_files() |> as_tibble() # システムフォントの閲覧
+font_files() |> as_tibble() |>
+  filter(str_detect(ps_name, "NotoSansCJK")) |> 
+  select(file, face, ps_name) 
 
+# 埋め込みフォントの指定
+font_add(family = "notosansjp",
+         regular = "NotoSansCJKjp-Regular.otf")
+
+# フォントを有効にする
+theme_gray(base_family = "notosansjp") |> theme_set()
+
+# Windows の場合
+font_add(family = "meiryo", regular = "meiryo.ttc")
+theme_gray(base_family = "meiryo") |> theme_set()
+showtext_auto()
+################################################################
 
