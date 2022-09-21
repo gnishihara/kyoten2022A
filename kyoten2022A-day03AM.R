a2d395cbb0a672d42d6830b1df0dafa9036761af
Greg Nishihara <gnishihara@gmail.com>
2022-09-21 09:04:54 +0900

    update

--- kyoten2022A-day03AM.R
+++ kyoten2022A-day03AM.R
@@ -5,10 +5,7 @@
 # tidyverse の紹介
 library(tidyverse)
 library(lubridate)
-library(googlesheets4)
 
 
 
-"https://drive.google.com/drive/folders/1uJz6xqYGFw-_GWRg-VDWHKk3dtsZUF4O?usp=sharing"
 
-"https://drive.google.com/file/d/1Hb1QgP6mH3cx9TZSaiLiXYwYnB5G7gS3/view?usp=sharing"
