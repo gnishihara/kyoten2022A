423c71d75e7b6e7c35812950f933b8a7619b8539
Greg Nishihara <gnishihara@gmail.com>
2022-09-21 09:03:37 +0900

    update

---
+++ kyoten2022A-day03AM.R
@@ -0,0 +1,14 @@
+# 公開臨海データ解析実習　A日程
+# 2022 September 21 AM
+# Greg Nishihara
+
+# tidyverse の紹介
+library(tidyverse)
+library(lubridate)
+library(googlesheets4)
+
+
+
+"https://drive.google.com/drive/folders/1uJz6xqYGFw-_GWRg-VDWHKk3dtsZUF4O?usp=sharing"
+
+"https://drive.google.com/file/d/1Hb1QgP6mH3cx9TZSaiLiXYwYnB5G7gS3/view?usp=sharing"
